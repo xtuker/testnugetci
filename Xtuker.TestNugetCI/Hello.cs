7e12c4d382a4071afba853d9be1c4f630fb95668
Alexander Falkovsky <alr.fkv@gmail.com>
2024-07-22 22:16:37 +0300

    create project

---
+++ Xtuker.TestNugetCI/Hello.cs
@@ -0,0 +1,15 @@
+﻿namespace Xtuker.TestNugetCI;
+
+/// <summary>
+/// Hello world class
+/// </summary>
+public class Hello
+{
+    /// <summary>
+    /// Return the "Hello world!" string
+    /// </summary>
+    public string Say()
+    {
+        return "Hello world!";
+    }
+}
